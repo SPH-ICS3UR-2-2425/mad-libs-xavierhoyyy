cf3300f6b1df854d59726b072951c373dbf87837
MrZebarth <kris.zebarth@ocsb.ca>
2024-09-16 12:44:41 -0400

    Added three questions to the program
    Expanded on the example.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		/*
 		 * MadLibs Program
-		 * Author: 
+		 * Author: Mr. Zebarth
 		 * Class: ICS3U
 		 * Date: Sept. 16, 2024
 		 * Make a program that asks for 10 inputs
@@ -16,7 +16,12 @@ public class Main {
 		System.out.println("Welcome to Mad Libs!");
 		System.out.println("What's your name?");
 		String name = in.nextLine();
-		System.out.println("One day, "+name+" wrote a Mad Lib in class!");
+		System.out.println("Enter a number: ");
+		int num1 = in.nextInt();
+		System.out.println("Enter a place: ");
+		in.nextLine();//flush the buffer
+		String place = in.nextLine();
+		System.out.println("One day, "+name+" wrote "+num1+" Mad Libs in "+place+"!");
 
 	}
 
