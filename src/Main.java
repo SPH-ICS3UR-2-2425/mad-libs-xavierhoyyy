628d202929fc1eaa243b72b99933e8eea0ed4214
xavierhoyyy <xavier.hoy@stu.ocsb.ca>
2024-09-18 13:16:59 -0400

    Finished Project
    all 10 lines done with story

--- src/Main.java
+++ src/Main.java
@@ -14,22 +14,37 @@ public class Main {
 		
 		Scanner in = new Scanner(System.in);
 		System.out.println("Welcome to Mad Libs!");
-		System.out.println("What's your name?");
+		System.out.println("Enter your name: ");
 		String name = in.nextLine();
 		System.out.println("Enter a Number: ");
 		int num1 = in.nextInt();
-		in.nextLine();
+		in.nextLine();//flush
 		System.out.println("Enter a place: ");
 		String place = in.nextLine();  
 		System.out.println("Enter a date: ");
 		String date = in.nextLine();
 		System.out.println("Enter a time of day: ");
 		String time = in.nextLine();
+		System.out.println("Enter a decimal number: ");
+		double decimal = in.nextDouble();
+		in.nextLine();//flush
+		System.out.println("Enter a Phrase: ");
+		String phrase = in.nextLine();
+		System.out.println("Enter a meal: ");
+		String meal = in.nextLine();
+	    System.out.println("Enter your favourite number: ");
+		int num2 =in.nextInt();
+		in.nextLine();//flush
+		System.out.println("Enter a famous person: ");
+		String famous = in.nextLine();
+		
+		
+		
 		
 		
 		
 		//Story
-		System.out.println("One day, "+name+" Mad Libs in "+place+" at "+time+" on "+date+", "+name+" keeps mentioning that his favourite number is "+num1+"!");
+		System.out.println("One day, "+name+" Mad Libs in "+place+" at "+time+" on "+date+", "+name+" keeps mentioning that his favourite number is "+num1+". All " +name+ " really does is talk about "+num1+". "+name+" loves adding "+decimal+" to the end of his number "+name+". While in "+place+", "+name+" loves shouting out "+phrase+" While eating "+meal+". After eating and screaming "+name+" realised that his new favourite number is "+num2+". "+name+", after eating met "+famous+" and the started to scream "+num2+" at him, until he got a photo." );
 	}
 
 }
